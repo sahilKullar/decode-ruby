0bf417f09842d394ea31217d545324156d042e18
Sahil Kullar <sahil_kullar@outlook.com>
2025-01-12 23:28:11 +0530

    First Controller, Views, and Tests

---
+++ apps/standup/spec/views/activity/feed.html.tailwindcss_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'activity/feed.html.erb', type: :view do
+  it 'renders the word feed' do
+    render template: 'activity/feed'
+    expect(rendered).to match(/feed/)
+  end
+end
---
+++ apps/standup/spec/views/activity/mine.html.tailwindcss_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'activity/mine.html.erb', type: :view do
+  it 'renders the word mine' do
+    render template: 'activity/mine'
+    expect(rendered).to match(/mine/)
+  end
+end
